43285167f0f12b53ac9d5ca2c2be8ff47a09ea82
Sawyersmith824 <77470571+Sawyersmith824@users.noreply.github.com>
2021-09-07 20:56:16 -0400

    AdminBase fix
    Ability to save user to local storage moved from AdminBase to login.js so all login functions are central. AdminBase export kept so no need for extensive correction through out

--- keystone-database-project-restructure/src/admin/Index/Login/login.js
+++ keystone-database-project-restructure/src/admin/Index/Login/login.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import fire from 'constants/Fire';
 import './login.scss'
 import Button from '@material-ui/core/Button';
+import Admin from 'admin/Index/Admin';
+
 
 
 //simple login component that handles the given name
@@ -81,6 +83,47 @@ class Login extends Component {
         </div>
         // Button just makes button using Material UI
     );
+  }
+}
+
+class AdminBase extends Component {
+  constructor(props){
+      super(props);
+      // component of class AdminBase user is initailized as empty (get away from class components )
+      this.state ={
+          user:{},
+      }
+  }
+
+  //wrapper for authListener
+  //Listener waits for auth to happen after DidMount
+  componentDidMount(){
+      this.authListener();
+  }
+
+  //check if user is a member logs in successfully to firebase. 
+  //if yes load information to local storage so firebase can remember user
+  //if logging out remove information from local storage
+  authListener() {
+      fire.auth().onAuthStateChanged((user) => {
+          if(user) {
+              this.setState({user});
+             localStorage.setItem('user',user.uid);
+          } else {
+              this.setState({user:null});
+             localStorage.removeItem('user');
+          }
+      });
+  }// other comments basically cover but on Auth state change user info is saved to local storage as uid then removed on logout
+  
+
+  //if user is found in state, go to ACS, if no login
+  render() {
+      return (
+          <div>
+              {this.state.user ? (<Admin />): (<Login />)}
+          </div>
+      )
   }
 }
 
